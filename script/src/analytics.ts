b9995d5584b15e0ae672976f9282dd7f722e39e4
Jelly Terra <jellyterra@proton.me>
2024-09-17 02:43:55 +0800

    Implement access reporting.

---
+++ script/src/analytics.ts
@@ -0,0 +1,16 @@
+let uuid = ''
+
+function uuidGen() {
+    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c => (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16))
+}
+
+function initAnalytics() {
+    if (localStorage.getItem('uuid') === null) localStorage.setItem('uuid', uuidGen())
+
+    uuid = localStorage.getItem('uuid')
+}
+
+function reportAccess(host: string, deployTime: string) {
+    fetch(`${host}/api/v1/reportAccess?uuid=${uuid}&deploy_time=${deployTime}&target=${window.location.href}`).then(() => {
+    })
+}

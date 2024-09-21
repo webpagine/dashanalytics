85c3e94cead4b9365dbd8c33872cd27a5403dc55
Jelly Terra <jellyterra@proton.me>
2024-09-21 16:00:13 +0800

    Encode attribution strings into valid URI.

--- script/src/analytics.ts
+++ script/src/analytics.ts
@@ -11,6 +11,6 @@ function initAnalytics() {
 }
 
 function reportAccess(host: string, deployTime: string) {
-    fetch(`${host}/api/v1/reportAccess?uuid=${uuid}&deploy_time=${deployTime}&target=${window.location.href}&user_agent=${window.navigator.userAgent}`).then(() => {
+    fetch(`${host}/api/v1/reportAccess?uuid=${uuid}&deploy_time=${deployTime}&target=${encodeURIComponent(window.location.href)}&user_agent=${encodeURIComponent(window.navigator.userAgent)}`).then(() => {
     })
 }
